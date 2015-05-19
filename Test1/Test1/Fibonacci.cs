792a6c0cd882248a5c062c850cc3f071e3f7ce6e
AleKruz <alekruzv@gmail.com>
2015-05-18 21:05:54 -0600

    Envío Fibonacci

---
+++ Test1/Test1/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Fibonacci
+    {
+        public int Calcular(int n)
+        {
+          /*int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                 int temp = a;
+                 a = b;
+                 b = temp + b;
+            }
+            return a;*/
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 1;
+            }
+            return Calcular(n - 1) + Calcular(n - 2);
+        }
+    }
+}
--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -10,9 +10,27 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            /*System.Console.WriteLine("Hello World");
             System.Console.WriteLine("Goodbye");
             System.Console.ReadKey();
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Fibonacci.Calcular(i));
+            }
+            Console.ReadKey();*/
+
+            Fibonacci f = new Fibonacci();
+            System.Console.WriteLine(f.Calcular(1));
+            System.Console.WriteLine(f.Calcular(2));
+            System.Console.WriteLine(f.Calcular(3));
+            System.Console.WriteLine(f.Calcular(4));
+            System.Console.WriteLine(f.Calcular(5));
+            System.Console.WriteLine(f.Calcular(6));
+            System.Console.WriteLine(f.Calcular(7));
+            System.Console.WriteLine(f.Calcular(8));
+            System.Console.WriteLine(f.Calcular(9));
+            System.Console.WriteLine(f.Calcular(10));
+            Console.ReadKey();
         }
     }
 }
