2956db815fd4265489a3adfc03f6391bc962a37f
AleKruz <alekruzv@gmail.com>
2015-07-13 20:45:46 -0600

    TallerDeEntidades

---
+++ TallerDeEntidades/ValidacionDeCondicionFinanciera/ValidadorDeCaracteristicasDeRedencion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerDeEntidades
+{
+    class ValidadorDeCaracteristicasDeRedencion
+    {
+        public bool CaracteristicasDeRedencionSonValidas(bool permiteRedencion, double comision, int periodoMinimoDeRetencionEnDias)
+        {
+            bool sonValidas = false;
+
+            if (permiteRedencion = false && comision == 0 && periodoMinimoDeRetencionEnDias == 0)
+            {
+                sonValidas = true;
+            }
+            else if (permiteRedencion = true && comision > 0 && periodoMinimoDeRetencionEnDias > 0)
+            {
+                sonValidas = true;
+            }
+            return sonValidas;
+        }
+    }
+}
---
+++ TallerDeEntidades/ValidacionDeCondicionFinanciera/ValidadorDeFechas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerDeEntidades
+{
+    public class ValidadorDeFechas
+    {
+        public bool FechaDeVigenciaEsValida(DateTime fechaDeVigencia,
+            DateTime fechaDeRegistro)
+        {
+            bool laFechaEsValida = false;
+
+            if (fechaDeVigencia >= fechaDeRegistro)
+            {
+                laFechaEsValida = true;
+            }
+            return laFechaEsValida;
+        }
+    }
+}
